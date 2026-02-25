d9d261f3b3efbdfcbc6f5c6d24642b5c0c089c06
iamDrini <valdrin.murselji@outlook.de>
2026-02-25 18:49:47 +0100

    fix - setAttribute theme

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,34 @@
 
 const buttonPlay = document.getElementById('play-btn');
-if(buttonPlay)
-buttonPlay.addEventListener("click", () => {
-    window.location.href = "./src/pages/settings.html";
-});
+if (buttonPlay)
+    buttonPlay.addEventListener("click", () => {
+        window.location.href = "./src/pages/settings.html";
+    });
 
 const buttonStart = document.getElementById('start-btn');
-if(buttonStart)
-buttonStart.addEventListener("click", () => {
-    window.location.href = "/src/pages/game.html";
-})
+if (buttonStart)
+    buttonStart.addEventListener("click", () => {
+        window.location.href = "/src/pages/game.html";
+    })
+
+function getInputValue(name: string) {
+    const valueRef = document.querySelector<HTMLInputElement>(`input[name="${name}"]:checked`);
+    if (!valueRef) {
+        return null;
+    }
+    const value = valueRef.value;
+    console.log(value);
+    if (name === "theme") {
+        localStorage.setItem("theme", value);
+    }
+}
+
+const themeContainer = document.querySelector('.input-container');
+if (themeContainer) {
+    themeContainer.addEventListener('change', (e) => {
+        const target = e.target as HTMLInputElement;
+        if (target.name === "theme") {
+            getInputValue("theme");
+        }
+    });
+}
--- src/scripts/game.ts
+++ src/scripts/game.ts
@@ -1,10 +1,18 @@
+function getThemeStorage() {
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme) {
+        document.documentElement.setAttribute("data-theme", savedTheme);
+        console.log(document.documentElement.getAttribute("data-theme"));
+    }
+}
+
 function renderCards() {
     let cardRef = document.getElementById('card-container');
     if (cardRef)
         cardRef.innerHTML = " ";
     for (let i = 0; i < 16; i++) {
-        if(cardRef)
-        cardRef.innerHTML += getCardTemplate();
+        if (cardRef)
+            cardRef.innerHTML += getCardTemplate();
     }
 }
 
