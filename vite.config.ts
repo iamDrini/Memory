a955267ca3ae4f3a65ad1ee45435a2f6ff90c4b8
iamDrini <valdrin.murselji@outlook.de>
2026-03-09 00:51:36 +0100

    fix-build mulitpage vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,13 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: "./",
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        game: 'src/pages/game.html',
+        settings: 'src/pages/settings.html',
+      },
+    },
+  },
 });
